98f439102f4010c8af8b2ac43308547b06c57cac
Anh Thư Phạm <thupham@Anhs-MacBook-Air.local>
2021-10-05 20:39:49 +0700

    final

--- src/components/Game.js
+++ src/components/Game.js
@@ -9,12 +9,10 @@ const Game = () => {
     const [stepNo, setStepNo] = useState(0);
     const [sort, setSort] = useState(true);
 
-
-    const winner = calculateWinner(history[stepNo].squares, size, history[stepNo].picked);
+    let winner = calculateWinner(history[stepNo].squares, size, history[stepNo].picked);
     let winnerCell;
 
-
-
+    //Handle when clicking on the board
     const handleClick = (i) => {
         const passMoves = history.slice(0, stepNo + 1);
         const current = [...passMoves[stepNo].squares];
@@ -27,8 +25,8 @@ const Game = () => {
         setStepNo(passMoves.length);
     };
 
+    //History moves
     let moves = history.map((step, move) => {
-
         const desc = move ? 'Go to move #' + move + ' : (' +
             (step.picked % size + 1) + ',' + (Math.floor(step.picked / size) + 1) + ')'
             : 'Go to game start';
@@ -40,46 +38,48 @@ const Game = () => {
         );
     });
 
-
+    //Handle when click "Submit" to re-size
     const handleSubmit = (event) => {
         event.preventDefault();
         setSize(Number(event.target[0].value));
         setHistory([{ squares: Array(size * size).fill(null), picked: null, }])
-
+        setXIsNext(true);
+        setStepNo(0);
     }
 
 
     let form = <form onSubmit={handleSubmit}>
         <label>
             Size:
-            <input type="number" name="size" />
+            <input type="number" name="size" max="20" min="5"/>
         </label>
-        <input type="submit" value="Submit" />
+        <input className="button" type="submit" value="Submit" />
     </form>
 
     const oppOrder = sort ? 'Descending' : 'Ascending';
     let toggleButton = <div>
         Toggle moves order to:
-        <button className="order" onClick={() => toggleOrder()}>{oppOrder}</button>
+        <button className="button" onClick={() => toggleOrder()}>{oppOrder}</button>
     </div>
 
     if(winner){
         winnerCell = winner.index;
+        console.log(winnerCell);
     }
     
 
     const toggleOrder = () => {
         setSort(!sort);
     }
+
     const jumpTo = (step) => {
         setStepNo(step);
         setXIsNext(step % 2 === 0);
     };
 
-
+    //Set status
     const evaluateStatus = () => {
         let status;
-
         if (winner) {
             status = 'Winner: ' + winner.player;
         }
@@ -93,9 +93,6 @@ const Game = () => {
         }
         return status;
     };
-
-
-
 
     return (
         <div className="game">
--- src/winner.js
+++ src/winner.js
@@ -1,34 +1,33 @@
 export function calculateWinner(squares, size, picked) {
+
     let row = Math.floor(picked / size);
     let col = picked % size;
 
-
+// Check row
+    let k = picked + 1;
     let result = [];
     result.push(picked);
-    let k = picked + 1;
-
-
-
-    // Check row
     while (k < row * size + size && squares[k] === squares[picked]) {
         result.push(k);
-        k++
+        console.log('Gia trị k: ' + k);
+        k++;
     }
+
     k = picked - 1;
     while (k >= row * size && squares[k] === squares[picked]) {
         result.push(k);
         k--;
     }
-    
-    if (result.length > 4){
-        return{
-            player: squares[picked],
-            index:result,
-        };
-    }
 
-    // Check col
-    result = []; 
+
+    if (result.length > 4)
+        return {
+            player: squares[picked],
+            index: result
+        };
+
+// Check col
+    result = [];
     result.push(picked);
     let h = picked + size;
     while (k < size * size && squares[h] === squares[picked]) {
@@ -40,16 +39,15 @@ export function calculateWinner(squares, size, picked) {
         result.push(h);
         h = h - size;
     }
-    
-    if (result.length > 4){
-        return{
+    if (result.length > 4)
+        return {
             player: squares[picked],
-            index:result,
+            index: result
         };
-    }
-    
-    // Check diagonal left
-    result = [];  
+
+
+// check diagonal left
+    result = [];
     result.push(picked);
     h = row + 1;
     k = col + 1;
@@ -59,22 +57,20 @@ export function calculateWinner(squares, size, picked) {
         k++;
     }
     h = row - 1;
-    h = col - 1;
+    k = col - 1;
     while (h >= 0 && k >= 0 && squares[h * size + k] === squares[picked]) {
         result.push(h * size + k);
         h--;
         k--;
     }
-    
-    if (result.length > 4){
-        return{
+    if (result.length > 4)
+        return {
             player: squares[picked],
-            index:result,
+            index: result
         };
-    }
 
 
-    // Check diagonal right 
+//Check diagonal right
     result = [];
     result.push(picked);
     h = row + 1;
@@ -91,13 +87,12 @@ export function calculateWinner(squares, size, picked) {
         h--;
         k++;
     }
-    
-    if (result.length > 4){
-        return{
+    if (result.length > 4)
+        return {
             player: squares[picked],
-            index:result,
+            index: result
         };
-    }
-   return null;
-    
+
+    return null;
+
 };
